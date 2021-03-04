578853d71aacbfe3d3e8c1e0c87526c07366e9ba
Santosh Mondal <santosh.ece06@gmail.com>
2021-03-04 13:01:37 +0530

    loadbalancer.

--- src/main/java/in/webinar/Day3ApiGateway1Application.java
+++ src/main/java/in/webinar/Day3ApiGateway1Application.java
@@ -2,8 +2,10 @@ package in.webinar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Day3ApiGateway1Application {
 
 	public static void main(String[] args) {
